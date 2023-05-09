6ad5b1962091ced6b8b54dec2b9d17c317c95435
graziele <graziele.oliramos@gmail.com>
2023-05-09 10:37:11 -0300

    exercício calcula produto python

---
+++ calculaproduto.py
@@ -0,0 +1,17 @@
+# Entrada de Dados
+
+num1 = input("Digite o primeiro número: ")
+num1 = int(num1)
+
+num2 = input("Digite o segundo número: ")
+num2 = int(num2)
+
+# Processamento 
+
+mult = num1 * num2 
+
+# Saída da Informação 
+
+print()
+
+print("A multiplicação de", num1, "por", num2, "é", mult)
